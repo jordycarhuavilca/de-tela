0473fa9b34876d9bce84042aba7585e69cc08647
Jordy carhuavilca <jordycarhuavilca2021@gmail.com>
2023-04-08 14:27:12 -0500

    agregando funciones de cambios de imagenes

---
+++ assets/js/imgs.js
@@ -0,0 +1,9 @@
+
+export const imgs = [
+    {img:'https://th.bing.com/th/id/OIP.OIJDRce0fQnRc0B8wzvIfgHaME?pid=ImgDet&rs=1'},
+    {img:'https://static.zara.net/photos///2021/I/0/1/p/8417/814/800/2/w/750/8417814800_6_1_1.jpg?ts=1631005081139'}
+]
+export const imgPalazos = [
+    {img:'https://i.pinimg.com/564x/b9/fd/8c/b9fd8c1b9a0659ead177034bd5dc2f63.jpg'},
+    {img:'https://i.pinimg.com/564x/42/6a/5e/426a5e195758eaa3fba1459c22c984d8.jpg'}
+]
--- assets/js/novedades.js
+++ assets/js/novedades.js
@@ -1,9 +1,37 @@
-const titulosNovedades = document.querySelector(".novedades__titulos")
-const titulos = ['joggers','palazos']
-function setTitulos(value){
-    return  value.map((titulo)=>`<span class="novedades__title-items mt-2">${titulo}</span>`)
+import {imgs,imgPalazos} from './imgs.js'
+const novedades_imgs = document.querySelector('.novedades-imgs')
+const novedadesTittle = document.querySelectorAll('.novedades__title-items')
+const nove__btnTittle_1 = document.querySelector('#nove__btnTittle_1')
+const nove__btnTittle_2 = document.querySelector('#nove__btnTittle_2')
+function escoger (img) {
+    novedades_imgs.innerHTML = ''
+  const container = img.map((x)=>{
+    return '<div class="nove__img1">' +
+   '<a href="#">' +
+   `<img src="${x.img}" alt="">` +     
+    '<div class="conta-novedades__btn-ver">' +
+    '<span class="novedades__description d-sm-block d-md-none">algodon</span>'+
+    '<a href="#" class="novedades__btn-ver">ver</a>'+ 
+    '</div> '
+    +
+    '</a>' + 
+    '</div>'
+  })
+  container.forEach((x)=>{
+      novedades_imgs.innerHTML += x
+  })
 }
-const jf =Array.from(setTitulos(titulos))
-jf.forEach((elemt) => {
-    titulosNovedades.innerHTML += elemt
+
+nove__btnTittle_1.addEventListener('click',(e)=>{
+    console.log(e.target.id)
+
+    if (e.target.id === 'nove__btnTittle_1') {
+        console.log('3')
+        escoger(imgs)
+    }
+} )
+nove__btnTittle_2.addEventListener('click',(e)=>{
+    if (e.target.id === 'nove__btnTittle_2') {
+        escoger(imgPalazos)
+    }
 })
